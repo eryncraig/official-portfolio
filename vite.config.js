c9d74a187706413e2c9d30999603cea42c2568fe
eryncraig <erynlcraig@gmail.com>
2024-07-17 12:09:28 -0700

    fixing vite compatibility

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/official-portfolio",
+  mode,
   plugins: [react()],
   css: {
     modules: {
@@ -11,3 +12,5 @@ export default defineConfig({
     }
   }
 })
+
+
