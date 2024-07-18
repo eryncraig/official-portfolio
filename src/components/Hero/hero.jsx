9edfc02618a005ea348895c4d836fc66cd7bbb4f
eryncraig <erynlcraig@gmail.com>
2024-07-18 16:31:12 -0700

    fixing small error

--- src/components/Hero/hero.jsx
+++ src/components/Hero/hero.jsx
@@ -7,7 +7,7 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Eryn</h1>
+        <h1 className={styles.title} id='hero'>Hi, I'm Eryn</h1>
         <p className={styles.description}>I’m a full-stack developer with 3 years
           of experience using React and NodeJS.
           Reach out if you’d like to learn more!</p>
